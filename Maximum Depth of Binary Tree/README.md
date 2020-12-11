fc726814a59a731fda0ed356636742e61ae277c6
Bahram Movlanov <movlan@users.noreply.github.com>
2020-12-10 20:02:55 -0700

    Maximum Depth of Binary Tree

---
+++ Maximum Depth of Binary Tree/README.md
@@ -0,0 +1,11 @@
+# [Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree/)
+
+## Instructions
+
+## Example
+
+Input:
+Output:
+
+Input:
+Output:
---
+++ Maximum Depth of Binary Tree/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  return !root ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+console.log(
+  maxDepth({
+    val: 3,
+    left: { val: 9, left: null, right: null },
+    right: {
+      val: 20,
+      left: { val: 15, left: null, right: null },
+      right: { val: 7, left: null, right: null },
+    },
+  })
+);
